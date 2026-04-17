c4865ef6c4e913992fd07c1952776570c5c10ab6
arely_ortega <oqarely15@gmail.com>
2026-04-17 16:49:21 -0600

    Add files via upload

---
+++ p03promediodetrescalificacaiones.py
@@ -0,0 +1,17 @@
+#arely ortega quiroz 
+# Definir tres calificaciones
+cal1 = 7
+cal2 = 5
+cal3 = 8
+
+# Calcular el promedio
+promedio = (cal1+cal2+cal3) / 3
+
+# Mostrar el promedio
+print("promedio:", promedio)
+
+# Verificar si esta aprobado o reprobado
+if promedio >=6:
+    print("¡aprobado!")
+else:
+    print("reprobado")
